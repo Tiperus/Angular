e96ab034dc4f1df10e4ed00a4a5daa38e743e0b6
Taras <tiperus@gmail.com>
2020-07-23 15:10:56 +0300

    add SingUp new User Edit & Delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CensorComponent } from './censor/censor.component';
+import { UserLoginDataComponent } from './user-login-data/user-login-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CensorComponent
+    CensorComponent,
+    UserLoginDataComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
